81249527acfb3b9c45b10010a80f3eb89684fd54
PedroHMoraes <pedroh.ti@hotmail.com>
2020-01-10 10:56:38 -0300

    Feita a conexão com o banco de dados

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Receita(models.Model):
+    nome_receita = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    tempo_prearo = models.IntegerField()
+    rendimento = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=100)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+receitas = {
+    1: 'Lasanha',
+    2: 'Sopa de Legumes',
+    3: 'Sorvete',
+    4: 'Caneloni',
+}
+
+dados = {
+    'nome_das_receitas' : receitas
+}
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
